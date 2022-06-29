bedbaeca1b206a3f2aac68e33892299bc3480f16
ArtP <artjom@panteam.net>
2022-06-29 06:48:45 +0300

    add AvaloniaTemplatedControlSample

---
+++ AvaloniaTemplatedControlSample/Views/MyTemplatedControl.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaTemplatedControlSample.Views
+{
+    public class MyTemplatedControl : TemplatedControl
+    {
+    }
+}
